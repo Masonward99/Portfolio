e328841c1d3790e2b66f2a2a293e5a2ff15076f2
Mason <mason@Masons-MacBook-Air-2.local>
2024-10-15 17:53:46 +0100

    styled form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,66 +8,69 @@ function Contact() {
     return (
       <div className="contact-container">
         <div className="contact">
-          <SectionHeading title={"Contact"} />
-          <div className="contact-content">
-            <form
-              className="contact-form"
-              name="contact"
-              method="POST"
-              data-netlify="true"
-              onSubmit="submit"
-                    >
-                <input type='hidden' name='form-name' value='contact'/>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                id="name"
-                className="contact-input"
-                name="name"
-                required
-              />
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                className="contact-input"
-                name="email"
-                required
-              />
-              <label>Message</label>
-              <textarea name="message" required />
-              <button type="submit" className="form-button">
-                Send
-              </button>
-            </form>
-            <div className="contact-links">
-              <h4>Email</h4>
-              <p>Masonward99@hotmail.com</p>
-              <h4>Phone</h4>
-              <p>07459 625 984</p>
-              <h4>Socials</h4>
-              <ul className="header-links">
-                <li>
-                  <a href="https://github.com/Masonward99" target="blank">
-                    <Github />
-                  </a>
-                </li>
-                <li>
-                  <a href="mailto: masonward99@hotmail.com" target="blank">
-                    <Email />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/mason-ward-177475170/"
-                    target="blank"
-                  >
-                    <Linkedin />
-                  </a>
-                </li>
-              </ul>
+            <SectionHeading title={"Contact"} />
+            <div className="contact-content">
+                <form
+                    className="contact-form"
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    onSubmit="submit"
+                >
+                    <input type='hidden' name='form-name' value='contact'/>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="contact-input"
+                        name="name"
+                        required
+                        placeholder='Enter your name'
+                                
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="contact-input"
+                        name="email"
+                        required
+                        placeholder='Enter your email'
+                    />
+                    <label>Message</label>
+                    <textarea name="message" required placeholder='Enter your message' className='form-textarea'/>
+                    <button type="submit" className="form-button">
+                        Send
+                    </button>
+                </form>
+                <div className="contact-links">
+                    <h4>Email</h4>
+                    <p>Masonward99@hotmail.com</p>
+                    <h4>Phone</h4>
+                    <p>07459 625 984</p>
+                    <h4>Socials</h4>
+                    <ul className="header-links">
+                        <li>
+                            <a href="https://github.com/Masonward99" target="blank">
+                                <Github />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto: masonward99@hotmail.com" target="blank">
+                                <Email />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/in/mason-ward-177475170/"
+                                target="blank"
+                            >
+                                <Linkedin />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </div>
         </div>
       </div>
     );
