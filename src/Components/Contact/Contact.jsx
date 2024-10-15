92845e16fd05f3dfcd752d3bb96a44ebf8e94f7e
Mason <mason@Masons-MacBook-Air-2.local>
2024-10-15 17:28:02 +0100

    fixed form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,47 +6,66 @@ import Email from '../../assets/Icons/Email';
 
 function Contact() {
     return (
-      <div className="contact-container" id='contact' method='POST' data-netlify='true' onSubmit='submit'>
+      <div className="contact-container">
         <div className="contact">
           <SectionHeading title={"Contact"} />
           <div className="contact-content">
-            <form className="contact-form" name='contact'>
-                <label htmlFor="name" >Name</label>
-                <input type="text" id="name" className='contact-input' name='name' required />
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" className='contact-input'name='email'required/>
-                <label>Message</label>
-                <textarea name='message' required/>
-                <button type="submit" className="form-button" >
-                    Send
-                </button>
+            <form
+              className="contact-form"
+              name="contact"
+              method="POST"
+              data-netlify="true"
+              onSubmit="submit"
+              id="contact"
+            >
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className="contact-input"
+                name="name"
+                required
+              />
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="contact-input"
+                name="email"
+                required
+              />
+              <label>Message</label>
+              <textarea name="message" required />
+              <button type="submit" className="form-button">
+                Send
+              </button>
             </form>
             <div className="contact-links">
-                <h4>Email</h4>
-                <p>Masonward99@hotmail.com</p>
-                <h4>Phone</h4>
-                <p>07459 625 984</p>
-                <h4>Socials</h4>
-                <ul className="header-links">
-                    <li>
-                        <a href="https://github.com/Masonward99" target="blank">
-                            <Github />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto: masonward99@hotmail.com" target="blank">
-                            <Email />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.linkedin.com/in/mason-ward-177475170/"
-                            target="blank"
-                        >
-                            <Linkedin />
-                        </a>
-                    </li>
-                </ul>
+              <h4>Email</h4>
+              <p>Masonward99@hotmail.com</p>
+              <h4>Phone</h4>
+              <p>07459 625 984</p>
+              <h4>Socials</h4>
+              <ul className="header-links">
+                <li>
+                  <a href="https://github.com/Masonward99" target="blank">
+                    <Github />
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto: masonward99@hotmail.com" target="blank">
+                    <Email />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/mason-ward-177475170/"
+                    target="blank"
+                  >
+                    <Linkedin />
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
